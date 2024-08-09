35ed589720491feef0cc8fcd974f51eaa7fe300d
sujatha-94 <sujathakarri701@gmail.com>
2024-08-09 16:47:15 +0530

    String

--- LongestWord.java
+++ LongestWord.java
@@ -1,3 +1,16 @@
 public class LongestWord{
 // Prepare a Logic to findout LongestWord in Sentence
+public static void main(String[] args) {
+    String sentence = "This is a longest word sentence";
+    String[] words = sentence.split("\\s+");
+    String longestWord = "";
+
+    for (String word : words) {
+        if (word.length() > longestWord.length()) {
+            longestWord = word;
+        }
+    }
+    System.out.println("Longest word: " + longestWord);
 }
+}
+
--- VowelsandConsonents.java
+++ VowelsandConsonents.java
@@ -1,3 +1,22 @@
 public class VowelsandConsonents{
 // Prepare a Logic to FindOut Vowels and Consosnents and Print them Spearately from a String
+public static void main(String[] args) {
+    String str = "Hello World";
+    String vowels = "";
+    String consonants = "";
+
+    for (char ch : str.toLowerCase().toCharArray()) {
+        if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+            vowels += ch;
+        } else if (ch >= 'a' && ch <= 'z') {
+            consonants += ch;
+        }
+    }
+
+    System.out.println("Vowels: " + vowels);
+    System.out.println("Consonants: " + consonants);
 }
+}
+
+
+
