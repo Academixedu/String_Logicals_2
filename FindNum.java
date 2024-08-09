4af9120645685940dc0db4191d9a565af2edb52c
Sarveshbramhane <139378858+Sarveshbramhane@users.noreply.github.com>
2024-08-09 23:44:14 +0530

    sarvesh

--- FindNum.java
+++ FindNum.java
@@ -4,4 +4,39 @@ public class FindNum{
   // output should be like
   // 1
   // 2
+
+void math1(String s){
+  for(int i=0;i<=s.length()-1;i++)
+  {
+    if(Character.isDigit(s.charAt(i)))
+    {
+System.out.println(s.charAt(i));
+    }
+     
+  }
+  System.out.println("--------------------");
+}
+void math2(String str){
+  
+  for(int i=0;i<=str.length()-1;i++){
+    char ch=str.charAt(i);
+    if(ch>='0'&& ch<='9'){
+      System.out.println(ch);
+    }
+
+  }
+
+}
+
+  public static void main(String[] args) {
+    String s="amsture4258";
+    String str="java5126";
+FindNum aobj=new FindNum();
+   aobj.math1(s);
+   aobj.math2(str);
+
+
+
+
+  }
 }
--- Occurence.java
+++ Occurence.java
@@ -1,4 +1,24 @@
 public class Occurence {
 // prepre a Logic to FindOut Occurence of Char in a String 
   // if we take 'l' to check occurence in String Hello World It Should return 3 as Output 
+
+public int occur(String s){
+int count=0;
+for(int i=0;i<=s.length()-1;i++){
+  if(s.charAt(i)=='t'){
+    count++;
+  }
+}
+return count;
+
+  
+}
+
+  public static void main(String[] args) {
+    String s="choko is male  toto";
+    Occurence obj=new Occurence();
+  obj.occur(s);
+    System.out.println("the ocuurence => "+obj.occur(s));
+
+  }
 }
