f83fdd16bba958b2454b7e92c47faf7f5297dc12
Bharathmanne18 <bharathmanne19@gmail.com>
2024-08-09 16:09:18 +0530

    Update Occurence.java

--- Occurence.java
+++ Occurence.java
@@ -1,4 +1,20 @@
+ import java.util.Scanner;
 public class Occurence {
 // prepre a Logic to FindOut Occurence of Char in a String 
   // if we take 'l' to check occurence in String Hello World It Should return 3 as Output 
+  public static void main(String args[]){
+    Scanner s= new Scanner(System.in);
+    System.out.println("enter a sentence");
+    String a=s.nextLine();
+    System.out.println("enter a character");
+    char ch = s.next().charAt(0);
+    int count=0;
+    for(char i:a.toCharArray())
+    {
+      if(i==ch){
+        count++;
+      }
+    }
+    System.out.println(count);
+  }
 }
