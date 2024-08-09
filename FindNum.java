aba55b6c5aff54c4758557c54b6a3aa4e40e9db3
Shashi preetham Nathi <nathishashipreetham@gmail.com>
2024-08-09 23:05:04 +0530

    Update FindNum.java

--- FindNum.java
+++ FindNum.java
@@ -1,7 +1,25 @@
-public class FindNum{
-// prepare a logic to find out numbers in a string and print them
-  // Ex if we take Java12Script
-  // output should be like
-  // 1
-  // 2
-}
+public class findnum {
+        // prepare a logic to find out numbers in a string and print them
+          // Ex if we take Java12Script
+          // output should be like
+          // 1
+          // 2
+            public static void main(String[] args) {
+                String str = "Java12Script";
+        
+                // Iterate through each character in the string
+                for (int i = 0; i < str.length(); i++) {
+                    char ch = str.charAt(i);
+        
+                    // Check if the character is a digit
+                    if (Character.isDigit(ch)) {
+                        System.out.println(ch);
+                    }
+                }
+            }
+        }
+        
+
+
+        
+
