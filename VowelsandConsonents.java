f688d430fca733084659ed9fa28da57df6468897
fayazshaik515 <fayazshaik231104@gmail.com>
2024-08-09 17:50:44 +0530

    from fayaz

--- VowelsandConsonents.java
+++ VowelsandConsonents.java
@@ -1,3 +1,28 @@
+import java.util.Scanner;
 public class VowelsandConsonents{
 // Prepare a Logic to FindOut Vowels and Consosnents and Print them Spearately from a String
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your string:");
+        String str = sc.nextLine();
+
+        String vowels = "";
+        String consonants = "";
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+
+            if ("aeiou".indexOf(ch) >= 0) {
+                vowels =vowels+ch;
+            } else if (ch >= 'a' && ch <= 'z') {
+                consonants=consonants + ch;
+             }
+        }
+
+        System.out.println("Vowels: " + vowels);
+        System.out.println("Consonants: " + consonants);
+
+    }
 }
+
