1e27553bc5c3ca342c0793ffc7245ecba71777e6
Bharathmanne18 <bharathmanne19@gmail.com>
2024-08-09 15:46:17 +0530

    Update FindNum.java

--- FindNum.java
+++ FindNum.java
@@ -1,7 +1,16 @@
+ import java.util.Scanner;
+
 public class FindNum{
-// prepare a logic to find out numbers in a string and print them
-  // Ex if we take Java12Script
-  // output should be like
-  // 1
-  // 2
+   public static void main(String args[])
+{
+// Prepare a Logic to findout LongestWord in Sentence
+Scanner s=new Scanner(System.in);
+System.out.println("enter a string");
+String a=s.nextLine();
+for(char i:a.toCharArray()){
+  if(Character.isDigit(i)){
+    System.out.println(i);
+  }
+}
+}
 }
