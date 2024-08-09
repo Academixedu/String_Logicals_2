b01489356455a14ece120ca0f7e6e6e25467fb35
ratan-17 <rkmule2002@gmail.com>
2024-08-09 23:30:38 +0530

    Ratan!!

--- Occurence.java
+++ Occurence.java
@@ -1,4 +1,31 @@
-public class Occurence {
+public class Occurence
+{
 // prepre a Logic to FindOut Occurence of Char in a String 
   // if we take 'l' to check occurence in String Hello World It Should return 3 as Output 
+
+int meth1(String s)
+{
+  int count=0;
+
+  for (int i=0;i<=s.length()-1;i++)
+  {
+    if(s.charAt(i)=='e')
+    {
+      count++;
+    }
+  }
+
+  return count;
+}
+
+
+public static void main(String[] args) 
+  {
+    String s="Lets play togetheeer";
+    Occurence obj=new Occurence();
+   int result= obj.meth1(s);
+   System.out.println("The count of word that you have entered is : "+result);
+  }
+
+
 }
