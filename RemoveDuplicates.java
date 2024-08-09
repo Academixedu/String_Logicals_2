864743c6433353a7c1168517f7c93b6d8ed1a8be
Shashi preetham Nathi <nathishashipreetham@gmail.com>
2024-08-09 23:16:25 +0530

    Update RemoveDuplicates.java

--- RemoveDuplicates.java
+++ RemoveDuplicates.java
@@ -1,4 +1,30 @@
-public class RemoveDuplicates{
-// Prepare a Logic to find out and Remove Duplicate char in a String
-  // Ex Hello World should be printed as Helo Wrd
+
+    import java.util.HashSet;
+
+public class duplicate{
+    public static void main(String[] args) {
+        String str = "Hello World";
+        String result = removeDuplicates(str);
+        System.out.println("String after removing duplicates: " + result);
+    }
+
+    public static String removeDuplicates(String str) {
+        HashSet<Character> seen = new HashSet<>(); // To track seen characters
+        StringBuilder sb = new StringBuilder();
+
+        // Iterate through each character in the string
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+
+            // If the character has not been seen yet, append it to the StringBuilder
+            if (!seen.contains(ch)) {
+                seen.add(ch);
+                sb.append(ch);
+            }
+        }
+
+        return sb.toString(); // Convert StringBuilder to String and return
+    }
 }
+
+
