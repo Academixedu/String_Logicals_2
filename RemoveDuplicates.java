cf9fe26fcaf8173c9bf44193cdbfdd600ff38e80
kavitha-guntru <kavithaguntru60@gmail.com>
2024-08-09 17:28:54 +0530

    kavitha

--- RemoveDuplicates.java
+++ RemoveDuplicates.java
@@ -1,4 +1,24 @@
-public class RemoveDuplicates{
-// Prepare a Logic to find out and Remove Duplicate char in a String
-  // Ex Hello World should be printed as Helo Wrd
+import java.util.Scanner;
+public class RemoveDuplicates 
+{
+  // Prepare a Logic to find out and Remove Duplicate char in a String
+  // Ex: Hello World should be printed as Helo Wrd
+  public static void main(String[] args) 
+  {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter sentence:");
+    String s = sc.nextLine();
+    String result = "";
+    for (int i = 0; i < s.length(); i++) 
+    {
+      char ch = s.charAt(i);
+      if (result.indexOf(ch) == -1) 
+      {
+        result += ch;
+      }
+    }
+      System.out.println(result);
+  }
 }
+  
+
--- VowelsandConsonents.java
+++ VowelsandConsonents.java
@@ -1,3 +1,30 @@
+import java.util.Scanner;
+
 public class VowelsandConsonents{
 // Prepare a Logic to FindOut Vowels and Consosnents and Print them Spearately from a String
+public static void main(String[] args) 
+{
+    Scanner sc =new Scanner(System.in);
+    System.out.println("enter sentence");
+    String s=sc.nextLine();
+    for(int i=0;i<s.length();i++)
+    {
+        char ch=s.charAt(i);
+        if (Character.isLetter(ch)) 
+        {
+            ch = Character.toLowerCase(ch);
+            if(ch=='A' || ch=='a' || ch=='E' || ch=='e' || ch=='I' || ch=='i' || ch=='O' || ch=='o' || ch=='U' || ch=='u')
+            {
+                System.out.println(ch+ " is a vowel");
+            }
+            else{
+                System.out.println(ch+ " is a consonant");
+            }
+        }
+    }
 }
+
+    
+    
+}
+
