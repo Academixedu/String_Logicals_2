2e257bf2e97d4b38c860d5161bea13ff68253efc
psvsai <pasupuletisvsm245@gmail.com>
2024-08-10 21:03:27 +0530

    PVSAI

--- FindNum.java
+++ FindNum.java
@@ -4,4 +4,20 @@ public class FindNum{
   // output should be like
   // 1
   // 2
+  public static void main(String[] args) {
+    String input = "Java12Script";
+    findNumbersInString(input); 
 }
+public static void findNumbersInString(String str) {
+    for (int i=0; i<str.length(); i++) {
+        char c = str.charAt(i); 
+        if (c >= '0' && c <= '9') {
+          System.out.println(c);
+        }
+      }
+  }
+  }
+  
+            
+          
+    
--- LongestWord.java
+++ LongestWord.java
@@ -1,3 +1,24 @@
 public class LongestWord{
 // Prepare a Logic to findout LongestWord in Sentence
+
+public static void main(String[] args) {
+    String sentence = "Atomic Habits By James Clear"; 
+    String longestWord = findLongestWord(sentence);             
+    System.out.println(" longest: " + longestWord);  
 }
+
+public static String findLongestWord(String sentence) {
+    String longestWord = "";
+    for (String word : sentence.split(" ")) {
+        if (word.length() > longestWord.length()) {
+            longestWord = word; 
+        }
+        else if(word.length()==longestWord.length()){
+            System.out.println("Longestnumber are:"+ word +" "+ longestWord);
+        }
+    }
+
+    return longestWord; 
+}
+}
+
