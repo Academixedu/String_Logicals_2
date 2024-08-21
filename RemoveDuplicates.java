0e77875a04adceb0545f7ce4550373c392d074b3
MekaManojKrishnalakshman <mekamanojkrishnalakshman@gmail.com>
2024-08-21 19:41:08 +0530

    logics strings

--- RemoveDuplicates.java
+++ RemoveDuplicates.java
@@ -1,4 +1,18 @@
+import java.util.Scanner;
 public class RemoveDuplicates{
 // Prepare a Logic to find out and Remove Duplicate char in a String
   // Ex Hello World should be printed as Helo Wrd
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    String str = sc.nextLine();
+    String str1 = "";
+    for (int i = 0; i < str.length(); i++) {
+      if (str1.indexOf(str.charAt(i)) == -1) {
+        str1 = str1 + str.charAt(i);
+      }
+    }
+    System.out.println(str1);
+    sc.close();
+  }
 }
--- VowelsandConsonents.java
+++ VowelsandConsonents.java
@@ -1,3 +1,22 @@
+import java.util.Scanner;
 public class VowelsandConsonents{
 // Prepare a Logic to FindOut Vowels and Consosnents and Print them Spearately from a String
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        String s1 = "";
+        String s2 = "";
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == 'a' || str.charAt(i) == 'e' || str.charAt(i) == 'i' || str.charAt(i) == 'o' || str.charAt(i) == 'u') {
+                s1 = s1 + str.charAt(i);
+            } else {
+                s2 = s2 + str.charAt(i);
+            }
+        }
+        System.out.println(s1);
+        System.out.println(s2);
+
+    }
 }
