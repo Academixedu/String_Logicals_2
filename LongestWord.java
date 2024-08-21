218c3095a22bf924cc997c79a0303247875c2cbb
Swapna-321 <gowdaperuswapna@gmail.com>
2024-08-21 20:08:40 +0530

    don

--- LongestWord.java
+++ LongestWord.java
@@ -1,3 +1,27 @@
 public class LongestWord{
 // Prepare a Logic to findout LongestWord in Sentence
+  public static void main(String args[])
+  {
+Scanner s=new Scanner(System.in);
+System.out.println("enter a sentence");
+String a=s.nextLine();
+int large=0;
+String largest_word="";
+for(String i:a.split(" "))//she is beautiful
+{
+    if(i.length()>large){
+
+        large=i.length();
+        largest_word=i;
+
+    }
+
+
+}
+
+System.out.println(largest_word);
+
+}
+
+
 }
