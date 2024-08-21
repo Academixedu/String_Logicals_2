fdea883125624e6691c48ce4f46a27433a5191cf
Swapna-321 <gowdaperuswapna@gmail.com>
2024-08-21 20:05:54 +0530

    done

--- FindNum.java
+++ FindNum.java
@@ -4,4 +4,13 @@ public class FindNum{
   // output should be like
   // 1
   // 2
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    String str = sc.nextLine();
+    for (int i = 0; i < str.length(); i++) {
+      if (Character.isDigit(str.charAt(i))) 
+        System.out.println(str.charAt(i));
+    } 
+    sc.close();
+  }
 }
