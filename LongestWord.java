5b2703f47ad3a7eb9e4adb0b647a624088649d12
chdkumar <chokkakulakumar1123@gmail.com>
2024-08-21 19:21:45 +0530

    StringLogics

--- LongestWord.java
+++ LongestWord.java
@@ -1,3 +1,21 @@
+import java.util.Scanner;
+
 public class LongestWord{
 // Prepare a Logic to findout LongestWord in Sentence
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        String[] arr = str.split(" ");
+        int max = 0;
+        String l = "";
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i].length() > max){
+                max = arr[i].length();
+                l = arr[i];
+            }
+        }
+        System.out.println(l+" "+max);
+        sc.close();
+    }
 }
--- Occurence.java
+++ Occurence.java
@@ -1,4 +1,19 @@
+import java.util.Scanner;
 public class Occurence {
 // prepre a Logic to FindOut Occurence of Char in a String 
   // if we take 'l' to check occurence in String Hello World It Should return 3 as Output 
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    String str = sc.nextLine();
+    String str1 = sc.nextLine();
+    int count = 0;
+    for (int i = 0; i < str.length(); i++) {
+      if (str.charAt(i) == str1.charAt(0)) {
+        count++;
+      }
+    }
+    System.out.println(count);
+    sc.close();
+  }
 }
