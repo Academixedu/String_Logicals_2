67c286cf16c40f0f27b17e4af805c8bb2550b540
gsbhavani <suryagogadi02@gmail.com>
2024-08-21 19:04:50 +0530

    completed

--- VowelsandConsonents.java
+++ VowelsandConsonents.java
@@ -1,3 +1,19 @@
+import java.util.Scanner;
 public class VowelsandConsonents{
+    public static void main(String args[]){
 // Prepare a Logic to FindOut Vowels and Consosnents and Print them Spearately from a String
+Scanner s= new Scanner(System.in);
+    System.out.println("enter a sentence");
+    String a=s.nextLine();
+    for(char ch:a.toCharArray())
+{
+    if(ch =='A' || ch=='a'|| ch=='E' || ch=='e' || ch=='i' || ch=='I' || ch=='o' || ch=='O' || ch=='u' || ch=='U'){
+
+    System.out.println(ch+ " is a vowel");
+}
+else{
+    System.out.println(ch+ " is a consonant");
+}
+}
+}
 }
