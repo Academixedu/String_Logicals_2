992b06e8c1da98c1a4e9e67cce0da3963165bc38
saipradeep484 <saipradeep484@gmail.com>
2024-08-21 22:46:21 +0530

    _assignment_updatesd

--- FindNum.java
+++ FindNum.java
@@ -1,7 +1,20 @@
+import java.util.*;
 public class FindNum{
 // prepare a logic to find out numbers in a string and print them
   // Ex if we take Java12Script
   // output should be like
   // 1
   // 2
+  public static void main(String[] args)
+  {
+    Scanner sc = new Scanner(System.in);
+    String s = sc.nextLine();
+    char arr[] = s.toCharArray();
+    for(int i=0;i<arr.length;i++)
+    {
+      if(arr[i]>='0' && arr[i]<='9')
+      System.out.print(arr[i]+" ");
+    }
+
+  }
 }
--- VowelsandConsonents.java
+++ VowelsandConsonents.java
@@ -1,3 +1,32 @@
 public class VowelsandConsonents{
+    public static void main(String[] args) {
+        String s1 = "Hello World";
+        String s = s1.toLowerCase();
+        String ss="";
+        for(int h=0;h<s.length();h++)
+        {
+            if(s.charAt(h)>='a' && s.charAt(h)<='z')
+            ss+=s.charAt(h);
+        }
+        char ovels[] = new char[s.length()];
+        char consosnents [] = new char[s.length()];
+        for(int i=0;i<s.length();i++)
+        {
+            if(s.charAt(i)==('a')||s.charAt(i)=='e'||s.charAt(i)=='o'||s.charAt(i)=='u'||s.charAt(i)=='i')
+            ovels[i]=s.charAt(i);
+            else
+            consosnents[i]=s.charAt(i);
+        }
+        System.out.print("ovels are: ");
+        for(int j=0;j<ovels.length;j++)
+        System.out.print( ovels[j]+" ");
+        System.out.println();
+System.out.print("consonensts are: ");
+       for(int jj=0;jj<consosnents.length;jj++)
+       System.out.print(consosnents[jj]+ " ");
+
+      
+    }
 // Prepare a Logic to FindOut Vowels and Consosnents and Print them Spearately from a String
+    
 }
