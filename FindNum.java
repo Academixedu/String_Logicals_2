72854686e7afeca13c23871cfb2c507b9b5e8686
srinadh0405 <srinadh.nallabothula@email.com>
2024-08-21 20:29:09 +0530

    String Logics

--- FindNum.java
+++ FindNum.java
@@ -1,7 +1,27 @@
+import java.util.Scanner;
+
 public class FindNum{
 // prepare a logic to find out numbers in a string and print them
   // Ex if we take Java12Script
   // output should be like
   // 1
   // 2
+
+    public static void main(String[] args) {
+
+      Scanner sc = new Scanner(System.in);
+      System.out.println("Enter the String:");
+      String str = sc.nextLine();
+
+      for(int i = 0; i<str.length();i++)
+      {  
+         if(Character.isDigit(str.charAt(i))){
+
+          System.out.println(str.charAt(i));
+         }
+
+      }
+    }
 }
+
+
--- Occurence.java
+++ Occurence.java
@@ -1,4 +1,26 @@
+import java.util.Scanner;
+
 public class Occurence {
 // prepre a Logic to FindOut Occurence of Char in a String 
   // if we take 'l' to check occurence in String Hello World It Should return 3 as Output 
-}
+
+ public static void main(String args[]){
+    Scanner s= new Scanner(System.in);
+    System.out.println("enter a sentence");
+    String a=s.nextLine();//srisyam
+    System.out.println("enter a character");
+    char ch = s.next().charAt(0);//s
+    int count=0;
+    for(char i:a.toCharArray())//['s','r']
+    {
+      if(i==ch){
+        count++;
+      }
+    }
+    System.out.println(count);
+    }
+  }
+
+
+
+  
