01b77f3b3a8b0d0bbd8cbb65099e1f2ca6a9ad12
Aparna526 <aparnakondapaturu@gmail.com>
2024-09-09 20:22:16 +0530

    three

--- RemoveDuplicates.java
+++ RemoveDuplicates.java
@@ -1,23 +1,47 @@
-import java.util.Scanner;
-
-public class RemoveDuplicates{
 // Prepare a Logic to find out and Remove Duplicate char in a String
   // Ex Hello World should be printed as Helo Wrd
 
-  public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    String str = sc.nextLine();
-    
-    for(int i=0; i<str.length(); i++){
-      char ch1 = str.charAt(i);
-      for(int j=0; j<str.length(); j++){
-        char ch2 = str.charAt(j);
+  
+import java.util.*;
 
-        if(ch1 == ch2){
-          
+class GFG 
+{
+    static String removeDuplicate(char str[], int n)
+    {
+        
+        int index = 0;
+
+        
+        for (int i = 0; i < n; i++)
+        {
+
+
+            int j;
+            for (j = 0; j < i; j++) 
+            {
+                if (str[i] == str[j])
+                {
+                    break;
+                }
+            }
+
+            
+            if (j == i) 
+            {
+                str[index++] = str[i];
+            }
         }
-      }
-  }
+        return String.valueOf(Arrays.copyOf(str, index));
+    }
 
+    // Driver code
+    public static void main(String[] args)
+    {
+        char str[] = "geeksforgeeks".toCharArray();
+        int n = str.length;
+        System.out.println(removeDuplicate(str, n));
+    }
 }
-}
+
+
+
