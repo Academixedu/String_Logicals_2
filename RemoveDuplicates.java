6586b70adc7575f2ca4ba26f322a7b1fd70c986d
kumar jyothiranjan <108728582+jyothiranjan25@users.noreply.github.com>
2024-09-09 19:54:58 +0530

    Update RemoveDuplicates.java

--- RemoveDuplicates.java
+++ RemoveDuplicates.java
@@ -1,4 +1,14 @@
 public class RemoveDuplicates{
-// Prepare a Logic to find out and Remove Duplicate char in a String
-  // Ex Hello World should be printed as Helo Wrd
+    // Prepare a Logic to find out and Remove Duplicate char in a String
+    // Ex Hello World should be printed as Helo Wrd
+    public static void main(String[] args) {
+        String str = "Hello World";
+        String result = "";
+        for(int i=0; i<str.length(); i++){
+            if(result.indexOf(str.charAt(i)) == -1){
+                result += str.charAt(i);
+            }
+        }
+        System.out.println("After Removing Duplicates: "+result);
+    }
 }
