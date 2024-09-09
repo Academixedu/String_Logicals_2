09af36e4a145f51a30a0b48c7d3c74af66891e19
Talari-Lavanya <lavanya62811@gmail.com>
2024-09-09 19:57:43 +0530

    Lavanya

--- FindNum.java
+++ FindNum.java
@@ -1,7 +1,26 @@
 public class FindNum{
 // prepare a logic to find out numbers in a string and print them
-  // Ex if we take Java12Script
+  // Example if we take Java12Script
   // output should be like
   // 1
   // 2
+  public static void printNumbersFromString(String inputString) {
+    
+    for (int i = 0; i < inputString.length(); i++) {
+        char ch = inputString.charAt(i);
+        
+        
+        if (Character.isDigit(ch)) {
+            System.out.println(ch);
+        }
+    }
+}
+
+public static void main(String[] args) {
+    
+    String inputString = "Java12Script";
+    
+   
+    printNumbersFromString(inputString);
+}
 }
--- VowelsandConsonents.java
+++ VowelsandConsonents.java
@@ -1,3 +1,41 @@
 public class VowelsandConsonents{
 // Prepare a Logic to FindOut Vowels and Consosnents and Print them Spearately from a String
+
+
+    
+    public static void separateVowelsAndConsonants(String inputString) {
+       
+        StringBuilder vowels = new StringBuilder();
+        StringBuilder consonants = new StringBuilder();
+
+        
+        inputString = inputString.toLowerCase();
+
+        
+        for (char ch : inputString.toCharArray()) {
+           
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                vowels.append(ch);
+            }
+            
+            else if (ch >= 'a' && ch <= 'z') {
+                consonants.append(ch);
+            }
+        }
+
+        
+        System.out.println("Vowels: " + vowels.toString());
+        System.out.println("Consonants: " + consonants.toString());
+    }
+
+    
+    public static void main(String[] args) {
+       
+        String inputString = "Hello World";
+        
+       
+        separateVowelsAndConsonants(inputString);
+    }
 }
+
+
