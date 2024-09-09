2b8d157cff51dc496afc6c5e91b5edd9e93f09bb
karthhiik <karthikthatikonda18@gmail.com>
2024-09-09 20:35:21 +0530

    Update LongestWord.java

--- LongestWord.java
+++ LongestWord.java
@@ -1,3 +1,23 @@
 public class LongestWord{
 // Prepare a Logic to findout LongestWord in Sentence
+  import java.util.*;
+public class LongestWord{
+// Prepare a Logic to findout LongestWord in Sentence
+ public static void main(String[] args) {
+        System.out.println("enter the worlds with space");
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        String rev = "";
+        String[] st = str.split(" ");
+        int max = 0;
+        for (int i = 0; i < st.length; i++) {
+            if (st[i].length() > max) {
+                max = st[i].length();
+                rev = st[i];
+            }
+        }
+        System.out.println(rev + ":" + max);
+
+    }
+}
 }
