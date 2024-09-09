654958e4b840cde5093d14f7f674eb5865b0ec2c
kurojulucky <luckykuroju28@gmail.com>
2024-09-09 20:37:04 +0530

    done

--- FindNum.java
+++ FindNum.java
@@ -1,4 +1,14 @@
 public class FindNum{
+  public static void main(String[] args) {
+    String str="java1234";
+    for(int i=0;i<str.length();i++){
+      char ch=str.charAt(i);
+      if(Character.isDigit(ch)){
+        System.out.println(ch);
+      }
+    }
+  }
+
 // prepare a logic to find out numbers in a string and print them
   // Ex if we take Java12Script
   // output should be like
--- LongestWord.java
+++ LongestWord.java
@@ -1,3 +1,17 @@
 public class LongestWord{
+    public static void main(String[] args) {
+        String Sentence="INDIA WON THE WORLDCUP";
+        System.out.println(findLargestWord(Sentence));
+    }
+    private static String findLargestWord(String Sentence){
+        String[] word=Sentence.split(" ");
+        String largestWord="";
+        for(int i=0;i<word.length;i++){
+            if(word[i].length()>largestWord.length()){
+                largestWord=word[i];
+            }
+        }
+        return largestWord;
+    }
 // Prepare a Logic to findout LongestWord in Sentence
 }
