b0271fab503f05d0aefe752dcff20e1362f7846e
revathivinnakota90@gmail.com <revathivinnakota90@gmail.com>
2024-09-09 20:12:47 +0530

    Revathi

--- Occurence.java
+++ Occurence.java
@@ -1,4 +1,20 @@
+
+
 public class Occurence {
-// prepre a Logic to FindOut Occurence of Char in a String 
-  // if we take 'l' to check occurence in String Hello World It Should return 3 as Output 
+    public static void main(String[] args) {
+
+    String s= "I am Revathi";
+    char c= 'a';
+    int Count =findCharOccurrences(s, c);
+    System.out.println("Occurrences of " +c+ ": " + Count);
+  }
+  public static int findCharOccurrences(String str, char targetChar) {
+    int count = 0;
+    for (int i = 0; i < str.length(); i++) {
+      if (str.charAt(i) == targetChar) {
+        count++; 
+      }
+    }
+    return count;
+  }
 }
