94edb223f6476b958584494b1c76548d14999857
Vijay123432 <vijaypenugoneda03@gmail.com>
2024-09-09 19:25:46 +0530

    longestword

--- LongestWord.java
+++ LongestWord.java
@@ -1,3 +1,27 @@
 public class LongestWord{
-// Prepare a Logic to findout LongestWord in Sentence
+// prepar a logic to findout longestword in sentence   
+    public static void main(String[] args) {
+        String str = "a logic to findout longestword in sentence ";
+
+        String lw = findLongestWord(str);
+        System.out.println("The longest word is: " + lw);
+    }
+
+    public static String findLongestWord(String str) {
+       String[] words = str.split(" ");
+        
+        String lw = "";
+
+        for (String word : words) {
+            if (word.length() > lw.length()) {
+              lw = word;
+            }
+        }
+        
+        return lw;
+    }
 }
+
+
+
+
