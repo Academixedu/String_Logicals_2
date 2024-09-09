72241a0b0e68f3208849b13c03aa5e1f0c61ab27
karthhiik <karthikthatikonda18@gmail.com>
2024-09-09 20:32:45 +0530

    Update VowelsandConsonents.java

--- VowelsandConsonents.java
+++ VowelsandConsonents.java
@@ -1,3 +1,26 @@
 public class VowelsandConsonents{
 // Prepare a Logic to FindOut Vowels and Consosnents and Print them Spearately from a String
+  import java.util.*;
+public class VowelsandConsonents{
+// Prepare a Logic to FindOut Vowels and Consosnents and Print them Spearately from a String
+public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        char[] ch = str.toCharArray();
+        String rev = "";
+        System.out.println("vowel::");
+        for (int i = 0; i < ch.length; i++) {
+            if (ch[i] == 'a' || ch[i] == 'e' || ch[i] == 'i' || ch[i] == 'o' || ch[i] == 'u')
+                System.out.println(ch[i]);
+            else {
+                rev += ch[i];
+            }
+        }
+        System.out.println("Non-vowel::");
+        for (int i = 0; i < rev.length(); i++) {
+            System.out.println(rev.charAt(i));
+        }
+
+    }
+}
 }
